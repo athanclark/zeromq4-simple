25b4e677a9332f745b20b53d0354374d5b241209
Athan Clark <athan.clark@gmail.com>
2018-08-15 11:00:02 -0600

    xpub

--- src/System/ZMQ4/Simple.hs
+++ src/System/ZMQ4/Simple.hs
@@ -53,54 +53,46 @@ class Sendable from to aux | from to -> aux where
 instance Sendable Pub Sub () where
   send Sub () s xs = Z.sendMulti s xs
 
+instance Sendable XPub Sub () where
+  send Sub () s xs = Z.sendMulti s xs
+
 instance Sendable Req Rep () where
   send Rep () s xs = Z.sendMulti s xs
 
 instance Sendable Req Router () where
-  send Router () s (x:|xs) = Z.sendMulti s ("" :| x:xs)
-    -- Z.send s [SendMore] ""
-    -- Z.send s [] x
+  send Router () s (x:|xs) = Z.sendMulti s ({-"" :|-} x:|xs)
 
 instance Sendable Router Req ZMQIdent where
   send Req (ZMQIdent addr) s (x:|xs) = Z.sendMulti s (addr :| "":x:xs)
-    -- Z.send s [SendMore] addr
-    -- Z.send s [SendMore] ""
-    -- Z.send s [] x
 
 
 -- | Receive a message over a ZMQ socket
 class Receivable from to aux | from to -> aux where
-  receive :: to -> Socket z from -> ZMQ z (Maybe (aux, (NonEmpty ByteString)))
+  receive :: to -> Socket z from -> ZMQ z (Maybe (aux, NonEmpty ByteString))
 
 instance Receivable Sub Pub () where
-  receive Pub s = do -- ((),) <$> Z.receive s
-    xs <- Z.receiveMulti s
-    case xs of
-      [] -> pure Nothing
-      (x:xs') -> pure (Just ((), x :| xs'))
+  receive Pub s = receiveBasic s
+
+instance Receivable Sub XPub () where
+  receive XPub s = receiveBasic s
 
 instance Receivable Req Rep () where
-  receive Rep s = do -- ((),) <$> Z.receive s
-    xs <- Z.receiveMulti s
-    case xs of
-      [] -> pure Nothing
-      (x:xs') -> pure (Just ((), x :| xs'))
+  receive Rep s = receiveBasic s
 
 instance Receivable Req Router () where
-  receive Router s = do
-    xs <- Z.receiveMulti s
-    case xs of
-      (_:x:xs') -> pure (Just ((), x :| xs'))
-      _ -> pure Nothing
-    -- _ <- Z.receive s
-    -- ((),) <$> Z.receive s
+  receive Router s = receiveBasic s
 
 instance Receivable Router Req ZMQIdent where
   receive Req s = do
     xs <- Z.receiveMulti s
-    -- addr <- ZMQIdent <$> Z.receive s
-    -- _ <- Z.receive s
-    -- x <- Z.receive s
     case xs of
       (addr:_:x:xs') -> pure (Just (ZMQIdent addr, x :| xs'))
       _ -> pure Nothing
+
+
+receiveBasic :: Z.Receiver t => Socket s t -> ZMQ s (Maybe ((), NonEmpty ByteString))
+receiveBasic s = do
+  xs <- Z.receiveMulti s
+  case xs of
+    (x:xs') -> pure (Just ((), x :| xs'))
+    _ -> pure Nothing

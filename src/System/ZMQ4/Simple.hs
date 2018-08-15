ffbb6ddd987401b2cab29fe0adf2624ea03e48dc
Athan Clark <athan.clark@gmail.com>
2018-08-14 20:37:42 -0600

    set random id

--- src/System/ZMQ4/Simple.hs
+++ src/System/ZMQ4/Simple.hs
@@ -3,6 +3,7 @@
   , FunctionalDependencies
   , GeneralizedNewtypeDeriving
   , DeriveGeneric
+  , TupleSections
   , OverloadedStrings
   #-}
 
@@ -10,11 +11,17 @@ module System.ZMQ4.Simple where
 
 import System.ZMQ4.Monadic
   ( ZMQ, Socket, Flag (SendMore)
-  , Req (..), Router (..))
+  , Req (..), Rep (..), Dealer (..), Router (..))
 import qualified System.ZMQ4.Monadic as Z
 
+import Data.Restricted (toRestricted)
 import Data.Hashable (Hashable)
 import Data.ByteString (ByteString)
+import qualified Data.UUID as UUID
+import Data.UUID.V4 (nextRandom)
+import qualified Data.ByteString.Lazy as LBS
+import Control.Monad.IO.Class (liftIO)
+
 import GHC.Generics (Generic)
 
 
@@ -24,9 +31,24 @@ import GHC.Generics (Generic)
 newtype ZMQIdent = ZMQIdent {getZMQIdent :: ByteString}
   deriving (Show, Eq, Ord, Generic, Hashable)
 
+setRandomIdentity :: Socket z from -> ZMQ z ()
+setRandomIdentity s = do
+  clientId <- liftIO nextRandom
 
+  case toRestricted $ LBS.toStrict $ UUID.toByteString clientId of
+    Nothing -> error "couldn't restrict uuid"
+    Just ident -> Z.setIdentity ident s
+
+
+
+-- * Classes
+
+-- | Send a message over a ZMQ socket
 class Sendable from to aux | from to -> aux where
   send :: to -> aux -> Socket z from -> ByteString -> ZMQ z ()
+
+instance Sendable Req Rep () where
+  send Rep () s x = Z.send s [] x
 
 instance Sendable Req Router () where
   send Router () s x = do
@@ -40,13 +62,17 @@ instance Sendable Router Req ZMQIdent where
     Z.send s [] x
 
 
+-- | Receive a message over a ZMQ socket
 class Receivable from to aux | from to -> aux where
   receive :: to -> Socket z from -> ZMQ z (aux, ByteString)
+
+instance Receivable Req Rep () where
+  receive Rep s = ((),) <$> Z.receive s
 
 instance Receivable Req Router () where
   receive Router s = do
     _ <- Z.receive s
-    (\x -> ((), x)) <$> Z.receive s
+    ((),) <$> Z.receive s
 
 instance Receivable Router Req ZMQIdent where
   receive Req s = do

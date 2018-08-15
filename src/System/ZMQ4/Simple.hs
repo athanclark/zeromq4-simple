3b2f340c8b8112532c3eafb82951bd3415c29822
Athan Clark <athan.clark@gmail.com>
2018-08-15 10:41:37 -0600

    using multi

--- src/System/ZMQ4/Simple.hs
+++ src/System/ZMQ4/Simple.hs
@@ -11,7 +11,9 @@ module System.ZMQ4.Simple where
 
 import System.ZMQ4.Monadic
   ( ZMQ, Socket, Flag (SendMore)
-  , Req (..), Rep (..), Dealer (..), Router (..))
+  , Req (..), Rep (..), Dealer (..), Router (..)
+  , Pub (..), Sub (..), XPub (..), XSub (..)
+  , Pull (..), Push (..))
 import qualified System.ZMQ4.Monadic as Z
 
 import Data.Restricted (toRestricted)
@@ -20,6 +22,7 @@ import Data.ByteString (ByteString)
 import qualified Data.UUID as UUID
 import Data.UUID.V4 (nextRandom)
 import qualified Data.ByteString.Lazy as LBS
+import Data.List.NonEmpty (NonEmpty (..))
 import Control.Monad.IO.Class (liftIO)
 
 import GHC.Generics (Generic)
@@ -45,38 +48,59 @@ setRandomIdentity s = do
 
 -- | Send a message over a ZMQ socket
 class Sendable from to aux | from to -> aux where
-  send :: to -> aux -> Socket z from -> ByteString -> ZMQ z ()
+  send :: to -> aux -> Socket z from -> NonEmpty ByteString -> ZMQ z ()
+
+instance Sendable Pub Sub () where
+  send Sub () s xs = Z.sendMulti s xs
 
 instance Sendable Req Rep () where
-  send Rep () s x = Z.send s [] x
+  send Rep () s xs = Z.sendMulti s xs
 
 instance Sendable Req Router () where
-  send Router () s x = do
-    Z.send s [SendMore] ""
-    Z.send s [] x
+  send Router () s (x:|xs) = Z.sendMulti s ("" :| x:xs)
+    -- Z.send s [SendMore] ""
+    -- Z.send s [] x
 
 instance Sendable Router Req ZMQIdent where
-  send Req (ZMQIdent addr) s x = do
-    Z.send s [SendMore] addr
-    Z.send s [SendMore] ""
-    Z.send s [] x
+  send Req (ZMQIdent addr) s (x:|xs) = Z.sendMulti s (addr :| "":x:xs)
+    -- Z.send s [SendMore] addr
+    -- Z.send s [SendMore] ""
+    -- Z.send s [] x
 
 
 -- | Receive a message over a ZMQ socket
 class Receivable from to aux | from to -> aux where
-  receive :: to -> Socket z from -> ZMQ z (aux, ByteString)
+  receive :: to -> Socket z from -> ZMQ z (Maybe (aux, (NonEmpty ByteString)))
+
+instance Receivable Sub Pub () where
+  receive Pub s = do -- ((),) <$> Z.receive s
+    xs <- Z.receiveMulti s
+    case xs of
+      [] -> pure Nothing
+      (x:xs') -> pure (Just ((), x :| xs'))
 
 instance Receivable Req Rep () where
-  receive Rep s = ((),) <$> Z.receive s
+  receive Rep s = do -- ((),) <$> Z.receive s
+    xs <- Z.receiveMulti s
+    case xs of
+      [] -> pure Nothing
+      (x:xs') -> pure (Just ((), x :| xs'))
 
 instance Receivable Req Router () where
   receive Router s = do
-    _ <- Z.receive s
-    ((),) <$> Z.receive s
+    xs <- Z.receiveMulti s
+    case xs of
+      (_:x:xs') -> pure (Just ((), x :| xs'))
+      _ -> pure Nothing
+    -- _ <- Z.receive s
+    -- ((),) <$> Z.receive s
 
 instance Receivable Router Req ZMQIdent where
   receive Req s = do
-    addr <- ZMQIdent <$> Z.receive s
-    _ <- Z.receive s
-    x <- Z.receive s
-    pure (addr, x)
+    xs <- Z.receiveMulti s
+    -- addr <- ZMQIdent <$> Z.receive s
+    -- _ <- Z.receive s
+    -- x <- Z.receive s
+    case xs of
+      (addr:_:x:xs') -> pure (Just (ZMQIdent addr, x :| xs'))
+      _ -> pure Nothing

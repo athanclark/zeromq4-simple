6d3e56ca04ac2c6a27639de97a70bfed8207af01
Athan Clark <athan.clark@gmail.com>
2018-08-14 20:22:39 -0600

    receive

--- src/System/ZMQ4/Simple.hs
+++ src/System/ZMQ4/Simple.hs
@@ -9,7 +9,8 @@
 module System.ZMQ4.Simple where
 
 import System.ZMQ4.Monadic
-  ( ZMQ, Socket, Flag (SendMore), Req (..), Router (..))
+  ( ZMQ, Socket, Flag (SendMore)
+  , Req (..), Router (..))
 import qualified System.ZMQ4.Monadic as Z
 
 import Data.Hashable (Hashable)
@@ -25,15 +26,31 @@ newtype ZMQIdent = ZMQIdent {getZMQIdent :: ByteString}
 
 
 class Sendable from to aux | from to -> aux where
-  send :: from -> to -> aux -> Socket z from -> ByteString -> ZMQ z ()
+  send :: to -> aux -> Socket z from -> ByteString -> ZMQ z ()
 
 instance Sendable Req Router () where
-  send Req Router () s x = do
+  send Router () s x = do
     Z.send s [SendMore] ""
     Z.send s [] x
 
 instance Sendable Router Req ZMQIdent where
-  send Router Req (ZMQIdent addr) s x = do
+  send Req (ZMQIdent addr) s x = do
     Z.send s [SendMore] addr
     Z.send s [SendMore] ""
     Z.send s [] x
+
+
+class Receivable from to aux | from to -> aux where
+  receive :: to -> Socket z from -> ZMQ z (aux, ByteString)
+
+instance Receivable Req Router () where
+  receive Router s = do
+    _ <- Z.receive s
+    (\x -> ((), x)) <$> Z.receive s
+
+instance Receivable Router Req ZMQIdent where
+  receive Req s = do
+    addr <- ZMQIdent <$> Z.receive s
+    _ <- Z.receive s
+    x <- Z.receive s
+    pure (addr, x)
